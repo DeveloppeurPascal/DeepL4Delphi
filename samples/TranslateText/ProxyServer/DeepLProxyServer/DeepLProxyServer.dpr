e36532c0d923c794841163c90b5a8277caf1ac38
patrick@premartin.fr <Patrick>
2022-01-24 17:30:33 +0100

    Proxy translate server as a Linux daemon

--- samples/TranslateText/ProxyServer/DeepLProxyServer/DeepLProxyServer.dpr
+++ samples/TranslateText/ProxyServer/DeepLProxyServer/DeepLProxyServer.dpr
@@ -2,6 +2,10 @@ program DeepLProxyServer;
 {$APPTYPE CONSOLE}
 
 uses
+{$IFDEF LINUX}
+  Posix.Stdlib, Posix.SysStat, Posix.SysTypes, Posix.Unistd, Posix.Signal,
+  Posix.Fcntl,
+{$ENDIF}
   System.SysUtils,
   System.Types,
   IPPeerServer,
@@ -147,6 +151,51 @@ begin
   end;
 end;
 
+{$IFDEF LINUX}
+
+const
+  // Missing from linux/StdlibTypes.inc !!! <stdlib.h>
+  EXIT_FAILURE = 1;
+  EXIT_SUCCESS = 0;
+
+var
+  pid: pid_t;
+  fid: Integer;
+  idx: Integer;
+  running: Boolean;
+
+procedure HandleSignals(SigNum: Integer); cdecl;
+begin
+  case SigNum of
+    SIGTERM:
+      begin
+        running := False;
+      end;
+    SIGHUP:
+      begin
+        // syslog(LOG_NOTICE, 'daemon: reloading config');
+        // Reload configuration
+      end;
+  end;
+end;
+
+procedure RunServerDaemon(APort: Integer);
+var
+  LServer: TIdHTTPWebBrokerBridge;
+begin
+  LServer := TIdHTTPWebBrokerBridge.Create(nil);
+  try
+    LServer.DefaultPort := APort;
+    StartServer(LServer);
+    while running do
+      sleep(100);
+  finally
+    LServer.Free;
+  end;
+end;
+
+{$ENDIF}
+
 var
   port: Integer;
   paramvalue: string;
@@ -190,15 +239,80 @@ begin
       DeepLSetAPIURL(CDeepLAPIURL_Pro)
     else
       DeepLSetAPIURL(CDeepLAPIURL_Free);
+{$IFDEF LINUX}
+// Want to understand how to create a Linux daemon ?
+// Look at Paolo Rossi blog :
+// https://blog.paolorossi.net/building-a-real-linux-daemon-with-delphi-part-1/
+// https://blog.paolorossi.net/building-a-real-linux-daemon-with-delphi-part-2/
+    if findcmdlineswitch('daemon') then
+    begin
+      // openlog(nil, LOG_PID or LOG_NDELAY, LOG_DAEMON);
+      try
+        if getppid() > 1 then
+        begin
+          pid := fork();
+          if pid < 0 then
+            raise exception.Create('Error forking the process');
 
-    try
-      if WebRequestHandler <> nil then
-        WebRequestHandler.WebModuleClass := WebModuleClass;
-      RunServer(port);
-    except
-      on E: exception do
-        Writeln(E.ClassName, ': ', E.Message);
+          if pid > 0 then
+            Halt(EXIT_SUCCESS);
+
+          if setsid() < 0 then
+            raise exception.Create
+              ('Impossible to create an independent session');
+
+          Signal(SIGCHLD, TSignalHandler(SIG_IGN));
+          Signal(SIGHUP, HandleSignals);
+          Signal(SIGTERM, HandleSignals);
+
+          pid := fork();
+          if pid < 0 then
+            raise exception.Create('Error forking the process');
+
+          if pid > 0 then
+            Halt(EXIT_SUCCESS);
+
+          for idx := sysconf(_SC_OPEN_MAX) downto 0 do
+            __close(idx);
+
+          fid := __open('/dev/null', O_RDWR);
+          dup(fid);
+          dup(fid);
+
+          umask(027);
+
+          chdir('/');
+        end;
+
+        if WebRequestHandler <> nil then
+          WebRequestHandler.WebModuleClass := WebModuleClass;
+
+        running := True;
+
+        RunServerDaemon(port);
+
+        ExitCode := EXIT_SUCCESS;
+      except
+        on E: exception do
+        begin
+          // syslog(LOG_ERR, 'Error: ' + E.Message);
+          ExitCode := EXIT_FAILURE;
+        end;
+      end;
+
+      // syslog(LOG_NOTICE, 'daemon stopped');
+      // closelog();
     end
+    else
+{$ENDIF}
+      try
+        if WebRequestHandler <> nil then
+          WebRequestHandler.WebModuleClass := WebModuleClass;
+        RunServer(port);
+      except
+        on E: exception do
+          Writeln(E.ClassName, ': ', E.Message);
+      end
   end;
 
 end.
